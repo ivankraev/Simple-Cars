bd504f6e6721644639d60055c557832519275469
Ivan Kraev <ivankraev2@abv.bg>
2022-03-03 16:39:46 +0200

    upgraded ux by setting dynamic time response on update functionality

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,83 +1,89 @@
-import React, { useEffect, useMemo } from 'react'
-import { useSelector } from 'react-redux'
-import { isEqual } from 'lodash'
-import MaterialTable from 'material-table'
-import { tableTitleColumns } from '../../common/tableTitleColumns'
-import { useCrudActions, useAuthActions } from '../../hooks/useActions'
-
+import React, { useEffect, useMemo } from "react";
+import { useSelector } from "react-redux";
+import { isEqual } from "lodash";
+import MaterialTable from "material-table";
+import { tableTitleColumns } from "../../common/tableTitleColumns";
+import { useCrudActions, useAuthActions } from "../../hooks/useActions";
+//CHECKS IF THE INPUT HAS NO EMPTY FIELDS
 const validateRow = (row) => {
-  //CHECKS IF THE INPUT HAS NO EMPTY FIELDS
-  return Object.values(row).length === tableTitleColumns.length
-}
+  return Object.values(row).length === tableTitleColumns.length;
+};
 
 const HomePage = () => {
   // ALL CRUD ACTIONS
-  const {
-    getCarsStart,
-    createCarStart,
-    deleteCarStart,
-    editCarStart,
-  } = useCrudActions()
-  const { setError } = useAuthActions()
+  const { getCarsStart, createCarStart, deleteCarStart, editCarStart } =
+    useCrudActions();
+  const { setError } = useAuthActions();
   //USER AND CARS INFORMATION
-  const token = useSelector((state) => state.login.user?.accessToken)
-  const { user } = useSelector((state) => state.login)
-  const { cars } = useSelector((state) => state.getAll)
+  const token = useSelector((state) => state.login.user?.accessToken);
+  const { user } = useSelector((state) => state.login);
+  const { cars } = useSelector((state) => state.getAll);
 
   //FIGURE OUT IF USER IS LOGGED IN AND LIFTING OWNED CARS ON TOP OF THE TABLE
   const sortCars = useMemo(() => {
-    let sortedCars = cars
+    let sortedCars = cars;
     if (user) {
-      const userCars = cars.filter((car) => car.user._id === user._id)
-      const otherCars = cars.filter((car) => car.user._id !== user._id)
-      sortedCars = [...userCars, ...otherCars]
+      const userCars = cars.filter((car) => car.user._id === user._id);
+      const otherCars = cars.filter((car) => car.user._id !== user._id);
+      sortedCars = [...userCars, ...otherCars];
     }
-    return sortedCars
-  }, [cars, user])
+    return sortedCars;
+  }, [cars, user]);
 
   useEffect(() => {
-    getCarsStart()
-  }, [])
+    getCarsStart();
+  }, []);
 
   return (
     <MaterialTable
       columns={tableTitleColumns}
       data={sortCars}
       title="Cars"
-      options={{ addRowPosition: 'first', actionsColumnIndex: -1 }}
+      options={{ addRowPosition: "first", actionsColumnIndex: -1 }}
       editable={{
+        isEditHidden: user ? (row) => row.user._id !== user._id : null,
+        isDeleteHidden: user ? (row) => row.user._id !== user._id : null,
         onRowAdd: user
           ? (rowData) =>
               new Promise((resolve) => {
-                resolve()
-
-                validateRow(rowData)
-                  ? createCarStart(token, user, rowData)
-                  : setError('Incorrect input', 'error')
+                setTimeout(() => {
+                  validateRow(rowData)
+                    ? createCarStart(token, user, rowData)
+                    : setError("Incorrect input", "error");
+                  resolve();
+                }, 800);
               })
           : null,
-        isEditHidden: user ? (row) => row.user._id !== user._id : null,
+
         onRowUpdate: user
           ? (newRowData, oldRowData) =>
-              new Promise((resolve) => {
-                resolve()
-                //CHECKS IF NEW AND OLD DATA ARE EQUAL AND RETURNS IF SO
-                const { tableData, ...otherProps } = oldRowData
-                if (isEqual(newRowData, { ...otherProps })) return
-                editCarStart(token, oldRowData._id, newRowData)
+              new Promise((resolve, reject) => {
+                const { tableData, ...otherProps } = oldRowData;
+                const isRowEqual = isEqual(newRowData, { ...otherProps });
+                const timer = isRowEqual ? 0 : 800;
+                setTimeout(() => {
+                  if (isRowEqual) {
+                    setError("Your input matches the current", "error");
+                    reject();
+                  } else {
+                    editCarStart(token, oldRowData._id, newRowData);
+                    resolve();
+                  }
+                }, timer);
               })
           : null,
-        isDeleteHidden: user ? (row) => row.user._id !== user._id : null,
         onRowDelete: user
           ? (selectedRow) =>
               new Promise((resolve) => {
-                resolve()
-                deleteCarStart(token, selectedRow)
+                setTimeout(() => {
+                  deleteCarStart(token, selectedRow);
+                  resolve();
+                }, 800);
               })
           : null,
       }}
     />
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
